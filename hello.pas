82741c94fb68e444cb2b272cb2d9420292bb8a86
Михаил Чугунов <mchugunov@sfedu.ru>
2024-10-13 12:19:49 +0300

    made hello.pas

---
+++ hello.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var n := ReadString('What language do you speak');
+  case n of
+    'English': PrintLn('Hello!');
+    'Русский': PrintLn('Здравствуйте!');
+    '日本語': PrintLn('こんにちは');
+  end;
+end.
